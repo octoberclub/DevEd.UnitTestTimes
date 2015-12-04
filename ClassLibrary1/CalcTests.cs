42d7980a1605dd292a706bdb4e36889f1495c26c
octoberclub <michelle.steele@15below.com>
2015-12-04 02:10:46 +0000

    Added CalcTests.cs

---
+++ ClassLibrary1/CalcTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    class CalcTests
+    {
+        [Test]
+        public async Task TestCalculator()
+        {
+            var sut = new Calculator();
+            int result= sut.Divide(10, 2);
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+
+    internal class Calculator
+    {
+        public int Divide(int  numerator, int denominator)
+        {
+            return numerator/denominator;
+        }
+    }
+}
