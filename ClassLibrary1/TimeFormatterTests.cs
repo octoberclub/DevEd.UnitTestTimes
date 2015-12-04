500929bc921c530226e2340124cf230ab4e5de15
octoberclub <michelle.steele@15below.com>
2015-12-04 01:20:31 +0000

    Added NowFormatter

---
+++ ClassLibrary1/TimeFormatterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+
+    public class TestClockDisplaysTests
+    {
+        [Test]
+        public void TestClockDisplaysNow()
+        {
+            var sut = new NowFormatter();
+
+            Assert.That(sut.ToString(), Is.EqualTo("09:15"));
+        }
+    }
+
+    public class NowFormatter
+    {
+        public override string ToString()
+        {
+            return DateTime.Now.ToString("hh:mm");
+        }
+    }
+}
