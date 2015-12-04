07b1fce5a336094a1f8ffa1b2b478d362aecd065
octoberclub <michelle.steele@15below.com>
2015-12-04 01:51:32 +0000

    Added TimerTests.cs

---
+++ ClassLibrary1/TimerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+public class TimeoutLogger
+{
+	[Test] public void TestTimer ()
+	{
+	    bool wasCalled=false;
+	    Start(()=>wasCalled=true);
+	    
+        Assert.IsTrue(wasCalled);
+	}
+
+    private void Start(Action a)
+    {
+        Task.Delay(10).ContinueWith((task => a()));
+    }
+}
